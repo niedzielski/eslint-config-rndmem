363b3971c0d26aaf11a851e131ae6a5b8dcb661d
Stephen Niedzielski <stephen@niedzielski.com>
2016-11-01 22:16:48 -0600

    allow imports and exports
    omit modules from node/no-unsupported-features. override to v7 since the
    engine auto detection doesn't work with ignores

--- .eslintrc.js
+++ .eslintrc.js
@@ -207,7 +207,7 @@ module.exports = {
     'node/no-unpublished-bin': 2,
     'node/no-unpublished-import': 2,
     'node/no-unpublished-require': 2,
-    'node/no-unsupported-features': 2,
+    'node/no-unsupported-features': [2, {version: 7, ignores: ['modules']}],
     'node/process-exit-as-throw': 2,
     'node/shebang': 2,
 
