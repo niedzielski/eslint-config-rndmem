3ab49cb816086e44efce541b59ad7bddf132c8bd
Stephen Niedzielski <stephen@niedzielski.com>
2017-01-04 20:44:01 -0700

    update: don't require complete sentences in jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -198,7 +198,6 @@ module.exports = {
     'jsdoc/check-tag-names': 1,
     'jsdoc/check-types': 1,
     'jsdoc/newline-after-description': [1, 'never'],
-    'jsdoc/require-description-complete-sentence': 1,
     'jsdoc/require-param-type': 1,
     'jsdoc/require-returns-type': 1,
 
